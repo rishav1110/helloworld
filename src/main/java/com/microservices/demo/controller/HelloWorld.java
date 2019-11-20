acc56d26e18ee8a7189f353e862e6c2fb718b054
Rishav <rishav.agrahari@walmartlabs.com>
2019-11-20 18:21:36 +0530

    Initial commit

---
+++ src/main/java/com/microservices/demo/controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.microservices.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello-world")
+    public String message()
+    {
+        return "Hello World";
+    }
+}
